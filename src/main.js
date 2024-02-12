88819445731e7dcdd1f11ef49a0a6f72e0f75d4b
PavloHohish <pasha.steam@ukr.net>
2024-02-12 21:47:53 +0200

    added js

---
+++ src/main.js
@@ -0,0 +1,28 @@
+(() => {
+  const mobileMenu = document.querySelector('.menu-container');
+  const openMenuBtn = document.querySelector('.open-menu');
+  const closeMenuBtn = document.querySelector('.close-menu');
+  const menuLinks = document.querySelectorAll('.mob-menu-link'); // Виберіть всі посилання в меню
+  const toggleMenu = () => {
+    const isMenuOpen =
+      openMenuBtn.getAttribute('aria-expanded') === 'true' || false;
+    openMenuBtn.setAttribute('aria-expanded', !isMenuOpen);
+    mobileMenu.classList.toggle('is-open');
+    const scrollLockMethod = !isMenuOpen
+      ? 'disableBodyScroll'
+      : 'enableBodyScroll';
+  };
+  menuLinks.forEach(menuLink => {
+    menuLink.addEventListener('click', () => {
+      toggleMenu(); // Закриваємо меню при кліку на посилання
+    });
+  });
+  openMenuBtn.addEventListener('click', toggleMenu);
+  closeMenuBtn.addEventListener('click', toggleMenu);
+  window.matchMedia('(min-width: 768px)').addEventListener('change', e => {
+    if (!e.matches) return;
+    mobileMenu.classList.remove('is-open');
+    openMenuBtn.setAttribute('aria-expanded', false);
+    bodyScrollLock.enableBodyScroll(document.body);
+  });
+})();
